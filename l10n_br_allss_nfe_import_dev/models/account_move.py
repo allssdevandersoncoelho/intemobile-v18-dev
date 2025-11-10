981dbd77172e9a50fb486fe6db8847b9852798fd
allssdevcaiqueanastacio <caique.anastacio@allss.com.br>
2025-11-10 11:06:26 -0300

    [FIX]

--- l10n_br_allss_nfe_import_dev/models/account_move.py
+++ l10n_br_allss_nfe_import_dev/models/account_move.py
@@ -1324,7 +1324,7 @@ class AllssAccountMoveNfeImport(models.Model):
     
 
     def action_post(self):
-        if self.l10n_br_allss_nf_status == "imported" and self.type == 'out_invoice':
+        if self.l10n_br_allss_nf_status == "imported" and self.move_type == 'out_invoice':
             move_lines = []
             for line in self.invoice_line_ids:
                 move_lines_values = {
