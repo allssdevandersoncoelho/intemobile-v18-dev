f70a56c268d9f9b2c15f77718c728fdb168ae8b5
allssdevcaiqueanastacio <caique.anastacio@allss.com.br>
2025-11-11 16:04:28 -0300

    [FIX]

--- l10n_br_allss_nfe_import_dev/models/account_move.py
+++ l10n_br_allss_nfe_import_dev/models/account_move.py
@@ -95,7 +95,7 @@ class AllssAccountMoveNfeImport(models.Model):
             pickings = self.env['stock.picking'].search([('l10n_br_allss_account_move_id', '=', r.id)])
             if pickings:
                 r.l10n_br_allss_picking_ids = [(6, 0, pickings.ids)]
-                r.l10n_br_allss_picking_count = len(pickings)
+                r.l10n_br_picking_count = len(pickings)
     
     l10n_br_allss_picking_type_id = fields.Many2one('stock.picking.type', default=default_l10n_br_allss_picking_type_id,
                                                      string='Tipo de Operação', copy=False, store=True,
