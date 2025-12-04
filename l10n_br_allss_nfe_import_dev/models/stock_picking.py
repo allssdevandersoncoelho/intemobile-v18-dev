6fbdedda36f9bf99d32c90a886f6db6265bc4bcf
allssdevcaiqueanastacio <caique.anastacio@allss.com.br>
2025-12-04 09:35:55 -0300

    [ADD]

--- l10n_br_allss_nfe_import_dev/models/stock_picking.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-import logging
-_logger = logging.getLogger(__file__)
-
-
-class StockPickingCustom(models.Model):
-    _inherit = 'stock.picking'
-
-    l10n_br_allss_account_move_id = fields.Many2one('account.move', 'Invoice', copy=False)
